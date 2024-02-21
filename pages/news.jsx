6d9c72d74aa3d3a47e90777e7eb78f2f9e22817d
maek95 <mekstrom01@gmail.com>
2024-02-21 13:18:06 +0100

    testing commit

--- pages/news.jsx
+++ pages/news.jsx
@@ -285,6 +285,7 @@ export default function News({ mainNews, rightSideNews, leftSideNews }) {
                         textDecoration: "none",
                       }}
                       // hejhejejhhjeehjhejehjhej
+                      // hej igen
                     >
                       {article.title}
                     </h2>
