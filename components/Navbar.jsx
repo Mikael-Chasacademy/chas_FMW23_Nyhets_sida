ae1feb92254c839dbbcbb55d35325363913f933f
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-02-26 11:31:13 +0100

    not much changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,62 +6,63 @@ import ThemeButton from "./ThemeButton";
 export default function Navbar() {
   return (
     <div>
-      
-    
-    <div
-      className={`navbar flex justify-between items-center p-3 bg-[#eeeff2ed] dark:bg-[#1A1C21] custom-thin-border-bottom transition-colors duration-200`}
-    >
-      <div className="flex justify-start">
-
-        <div className="flex gap-4">
-        <img
-          className="flex sm:hidden text-white fill-black dark:fill-white"
-          src="https://d33wubrfki0l68.cloudfront.net/79e75114856ae61628d2ad26504e3ff4ab2c71b6/f06a4/assets/svg/ham-menu.svg"
-          alt="hamburger menu"
-        /> 
-          <img alt="WIRED" class="ResponsiveImageContainer-eybHBd fptoWY responsive-image__image" src="https://www.wired.com/verso/static/wired/assets/logo-header.svg" srcset="" sizes="100vw"></img>
+      <div
+        className={`navbar flex justify-between items-center p-3 bg-[#eeeff2ed] dark:bg-[#1A1C21] custom-thin-border-bottom transition-colors duration-200`}
+      >
+        <div className="flex justify-start">
+          <div className="flex gap-4">
+            <span class="material-symbols-outlined">menu</span>
+          </div>
+          <div className="hidden sm:flex justify-start gap-3 items-center mx-4 ">
+            <Link
+              className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
+              href={"/"}
+            >
+              Home
+            </Link>{" "}
+            <Link
+              className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
+              href={"/politics"}
+            >
+              Politics
+            </Link>{" "}
+            <Link
+              className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
+              href={"/technology"}
+            >
+              Technology
+            </Link>{" "}
+            <Link
+              className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
+              href={"/sports"}
+            >
+              Sports
+            </Link>
+            {/* <Link href={"PoliticsTaBort"}>Politics</Link> */}{" "}
+          </div>
+          <div>
+          <img
+              alt="WIRED"
+              class="ResponsiveImageContainer-eybHBd fptoWY responsive-image__image"
+              src="https://www.wired.com/verso/static/wired/assets/logo-header.svg"
+              srcset=""
+              sizes="100vw"
+            ></img>
+          </div>
         </div>
-        <div className="hidden sm:flex justify-start gap-3 items-center mx-4 ">
-          <Link
-            className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
-            href={"/"}
-          >
-            Home
-          </Link>{" "}
-          <Link
-            className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
-            href={"/politics"}
-          >
-            Politics
-          </Link>{" "}
-          <Link
-            className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
-            href={"/technology"}
-          >
-            Technology
-          </Link>{" "}
-          <Link
-            className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
-            href={"/sports"}
-          >
-            Sports
-          </Link>
-          {/* <Link href={"PoliticsTaBort"}>Politics</Link> */}{" "}
+        <div className="flex justify-center items-center">
+          <div className="mx-2">
+            <Link
+              className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
+              href={"BookMarks"}
+            >
+              Saved
+            </Link>
+          </div>
+          <SubscribeButton />
+          <ThemeButton />
         </div>
       </div>
-      <div className="flex justify-center items-center">
-        <div className="mx-2">
-          <Link
-            className="text-decoration-line:none no-underline text-black dark:text-white hover:underline hover:text-sky-800"
-            href={"BookMarks"}
-          >
-            Saved
-          </Link>
-        </div>
-        <SubscribeButton />
-        <ThemeButton />
-      </div>
-    </div>
     </div>
   );
 }
